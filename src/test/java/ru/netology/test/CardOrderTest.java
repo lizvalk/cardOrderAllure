7e65d51208b083ec0ed660954e691a582155dad7
Lizvalk <lizvalk@mail.ru>
2023-11-04 11:31:33 +0700

    final

--- src/test/java/ru/netology/test/CardOrderTest.java
+++ src/test/java/ru/netology/test/CardOrderTest.java
@@ -1,9 +1,9 @@
 package ru.netology.test;
 
 import com.codeborne.selenide.Condition;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.Keys;
 import ru.netology.data.DataGenerator;
 
@@ -12,6 +12,14 @@ import java.time.Duration;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardOrderTest {
+    @BeforeAll
+    static void setUpAll () {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
     @BeforeEach
     void setUp () {
         open("http://localhost:9999/");
